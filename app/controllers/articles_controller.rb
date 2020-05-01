8487301d8847149098d2825f08d005c93d9e6f4b
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-30 21:43:03 -0500

    I create article list and all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,12 @@ class ArticlesController < ApplicationController
     def new
     end
 
+    def index
+        @articles = Article.all
+    end
+
     def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
+        @article = Article.new(article_params)
     
         @article.save
         redirect_to @article
@@ -12,4 +16,9 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
