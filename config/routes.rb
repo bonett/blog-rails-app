314419615cb7f16a184b349563ad60f8eb6cf1c6
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-01 01:06:22 -0500

    I installed two new gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   
+  get 'welcome/index'
+
+  get 'others/index', to: 'others#index'
+
   resources :articles do
     resources :comments
   end
