2f8d6338956c261c5aa0aba1e80a9c15451ff0cb
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-30 22:45:52 -0500

    I finished articles views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,20 @@
 class ArticlesController < ApplicationController
-    def new
-        @article = Article.new
-    end
-
     def index
         @articles = Article.all
     end
-
+    
+    def show
+        @article = get_article_by_id
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = get_article_by_id
+    end
+    
     def create
         @article = Article.new(article_params)
     
@@ -16,13 +24,32 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-
-    def show
-        @article = Article.find(params[:id])
+    
+    def update
+        @article = get_article_by_id
+    
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
-
+    
+    def destroy
+        @article = get_article_by_id
+        @article.destroy
+    
+        redirect_to articles_path
+    end
+    
     private
         def article_params
             params.require(:article).permit(:title, :text)
         end
+
+    private
+        def get_article_by_id
+            Article.find(params[:id])
+        end
+
 end
